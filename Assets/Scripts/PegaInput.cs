c6dea716612979fc82b8b45f3bea609fa7b7b022
William Santos <williamdsw@outlook.com>
2020-01-12 12:35:42 -0300

    Added Unity Project

---
+++ Assets/Scripts/PegaInput.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// DELEGATES
+public delegate void DelegateMethod (object sender, Somatoria somatoria);
+
+public class PegaInput : MonoBehaviour
+{
+    //--------------------------------------------------------------------//
+    // FIELDS 
+
+    // Config
+    private float cooldownTimer = 0.5f;
+
+    // State
+    private float horizontalCooldown = 0;
+    private float verticalCooldown = 0;
+
+    // Cached
+    private Somatoria somatoria;
+
+    // Delegates
+    private DelegateMethod Soma;
+
+    //--------------------------------------------------------------------//
+    // MONOBEHAVIOUR FUNCTIONS 
+
+    private void Start () 
+    {
+        somatoria = new Somatoria ();
+        somatoria.SetSoma (1000f);
+    }
+
+    private void Update () 
+    {
+        int horizontal = Mathf.RoundToInt (Input.GetAxisRaw ("Horizontal"));
+        int vertical = Mathf.RoundToInt (Input.GetAxisRaw ("Vertical"));
+        Vector2Int input = new Vector2Int (0, 0);
+
+        if (horizontal != 0)
+        {
+            input.x = GetInput (ref horizontalCooldown, horizontal);
+        }
+        else 
+        {
+            horizontalCooldown = 0;
+        }
+
+        if (vertical != 0)
+        {
+            input.y = GetInput (ref verticalCooldown, vertical);
+        }
+        else 
+        {
+            verticalCooldown = 0;
+        }
+
+        if (input != Vector2Int.zero)
+        {
+            if (input.x != 0)
+            {
+                Debug.Log("pushed x");
+                Soma += TesteSoma;
+            }
+
+            if (input.y != 0 && Soma != null)
+            {
+                Debug.Log("pushed y");
+                Soma (null, somatoria);
+            }
+        }
+    }
+
+    //--------------------------------------------------------------------//
+    // HELPER FUNCTIONS
+
+    private int GetInput (ref float cooldown, int axis)
+    {
+        if (Time.time > cooldown)
+        {
+            cooldown += (Time.time + cooldownTimer);
+            return axis;
+        }
+
+        return 0;
+    }
+
+    private void TesteSoma (object sender, Somatoria somatoria)
+    {
+        float soma = somatoria.GetSoma ();
+        soma += (soma * somatoria.GetJuros ());
+        somatoria.SetSoma (soma);
+        Debug.Log ("Soma: " + soma);
+    }
+}
---
+++ Assets/Scripts/Somatoria.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Somatoria
+{
+    //--------------------------------------------------------------------//
+    // FIELDS 
+
+    private float soma = 0;
+    private float juros = 0.15f;
+
+    //--------------------------------------------------------------------//
+    // GETTERS / SETTERS 
+
+    public float GetSoma ()
+    {
+        return this.soma;
+    }
+
+    public void SetSoma (float soma)
+    {
+        this.soma = soma;
+    }
+
+    public float GetJuros ()
+    {
+        return this.juros;
+    }
+}
